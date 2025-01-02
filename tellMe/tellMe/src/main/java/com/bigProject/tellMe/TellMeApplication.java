70016baecb80586c4ad2866593f1c833930d5320
taesik <0114mts@gmail.com>
2025-01-02 16:04:48 +0900

    base project/develop

---
+++ tellMe/tellMe/src/main/java/com/bigProject/tellMe/TellMeApplication.java
@@ -0,0 +1,13 @@
+package com.bigProject.tellMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TellMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TellMeApplication.class, args);
+	}
+
+}
